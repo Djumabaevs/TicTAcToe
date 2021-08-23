513092c9244dbd111e6c394305beee39b19ffd03
BakytDzhumabev <djumabaevb@gmail.com>
2021-08-23 16:03:07 +0600

    Initial Commit

---
+++ TicTAcToeTests/TicTAcToeTests.swift
@@ -0,0 +1,33 @@
+//
+//  TicTAcToeTests.swift
+//  TicTAcToeTests
+//
+//  Created by Bakyt Dzhumabaev on 23/8/21.
+//
+
+import XCTest
+@testable import TicTAcToe
+
+class TicTAcToeTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
